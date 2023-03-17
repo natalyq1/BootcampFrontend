049a0cad032158f3fab01d416d6a9955882f4081
natalyq1 <onatylili@gmail.com>
2023-03-17 16:02:23 -0500

    to do list completed

--- localStorage/ToDoList/localStorage.js
+++ localStorage/ToDoList/localStorage.js
@@ -1,15 +1,17 @@
 document.getElementById("btnCreate").addEventListener("click", function () {
   var inputForm = document.getElementById("inputForm").value;
   var cardsList = document.getElementById("cardsList");
-  
+
   localStorage.setItem("inputForm", inputForm);
 
   for (let i = 0; i < localStorage.length; i++) {
-    var key = localStorage.key(i);
-    cardsList.append( `${key}: ${localStorage.getItem(key)}\n`)
     
-    alert(`${key}: ${localStorage.getItem(key)}`);
-    
-    console.log(`${key}: ${localStorage.getItem(key)}`);
+   const newDiv = document.createElement("li"); 
+var key = localStorage.key(i);
+    const newContent = document.createTextNode(
+      `${localStorage.getItem(key)}\n`
+    );
+    newDiv.appendChild(newContent);
+    cardsList.appendChild(newDiv);
   }
 });
