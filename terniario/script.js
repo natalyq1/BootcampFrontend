87cab44530049d3f6706fbf21dcc96c2e2c0488a
natalyq1 <onatylili@gmail.com>
2023-03-28 10:05:28 -0500

    terniario concept

---
+++ terniario/script.js
@@ -0,0 +1,19 @@
+//operador ternario
+//(condicion) ? si se cumple : si no se cumple
+//var num = 11;
+
+//(num > 10) ? alert('es mayor a 10') : alert('Es menor a 10')
+
+//alert((num >10)?'Es mayor': 'Es menor')
+
+//var texto = (num>10)?'Es mayor': 'Es menor'
+//alert(texto)
+
+//operador ternario sin else
+//(num > 10) ? alert('es mayor a 10') : '';
+
+//ejercicio con ternario, identificar si es par o no
+//(num % 2 == 0) ? alert( 'es par') : alert('es impar') 
+
+var opcion =parseInt( prompt('ingrese 1, 2 o 3'));
+(opcion==1) ? alert('hola') :(opcion ==2) ? alert('como estas?') :(opcion==3) ? alert('chau') : ''
