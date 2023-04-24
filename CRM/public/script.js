9475fe6cc07d29954134709a850ff45df6524874
natalyq1 <onatylili@gmail.com>
2023-04-23 22:35:02 -0500

    some changes

--- CRM/public/script.js
+++ CRM/public/script.js
@@ -1,22 +1,38 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const tablaClientes = document.querySelector("#tabla-clientes tbody");
+document.addEventListener('DOMContentLoaded', () => {
+  // Obtener la tabla y el cuerpo de la tabla
+  const table = document.querySelector('table');
+  const tbody = document.querySelector('#customer-list');
 
-  fetch("/clientes")
-    .then((res) => res.json())
-    .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        let tr = document.createElement("tr");
-        let td1 = document.createElement("td");
-        let td2 = document.createElement("td");
-        let td3 = document.createElement("td");
-        td1.textContent = data[i].nombre;
-        td2.textContent = data[i].apellido;
-        td3.textContent = data[i].email;
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        document.querySelector("table").appendChild(tr);
-      }
+  // Hacer una solicitud a la ruta /clientes
+  fetch('/clientes')
+    .then(response => response.json())
+    .then(clientes => {
+      // Agregar cada cliente como una fila en la tabla
+      clientes.forEach(cliente => {
+        const row = document.createElement('tr');
+        const nombreCell = document.createElement('td');
+        const emailCell = document.createElement('td');
+        const propietarioCell = document.createElement('td');
+        const fechaCell = document.createElement('td');
+        const sintomasCell = document.createElement('td');
+
+        nombreCell.textContent = cliente.nombre;
+        emailCell.textContent = cliente.email;
+        propietarioCell.textContent = cliente.propietario;
+        fechaCell.textContent = cliente.fecha;
+        sintomasCell.textContent = cliente.sintomas;
+
+        row.appendChild(nombreCell);
+        row.appendChild(emailCell);
+        row.appendChild(propietarioCell);
+        row.appendChild(fechaCell);
+        row.appendChild(sintomasCell);
+        tbody.appendChild(row);
+      });
+      // Mostrar la tabla
+      table.style.display = 'table';
     })
-    .catch((err) => console.log(err));
+    .catch(error => {
+      console.error(error);
+    });
 });
