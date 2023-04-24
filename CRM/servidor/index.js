563c6efbe2da0a56fc112efca21101a366ef2358
natalyq1 <onatylili@gmail.com>
2023-04-23 21:48:05 -0500

    changes

--- CRM/servidor/index.js
+++ CRM/servidor/index.js
@@ -7,27 +7,28 @@ const app = express();
 // Configurar la conexión a la base de datos
 mongoose.connect('mongodb+srv://database:database@cluster1.5ld4yip.mongodb.net/crm?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Configurar el middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("../css", express.static("css"));
 // Importar el modelo de datos Cliente
 const Cliente = require('./models/Clientes');
 
 // Configurar las rutas de Express.js
 app.get('/', (req, res) => {
   // enviar el archivo HTML con la tabla
-  res.sendFile(path.join(__dirname, '../cliente/js/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+
 app.get('/clientes', (req, res) => {
-  // Hacer una consulta a la base de datos usando el modelo de datos Cliente
-  Cliente.find()
-    .then(clientes => {
-      // Enviar los datos de los clientes en formato JSON
-      res.json(clientes);
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).send('Error al obtener los clientes');
-    });
+  Cliente.find((error, result) => {
+    if (error) {
+      return console.log(error);
+    }
+    res.send(result);
+  });
 });
+
 
 // Iniciar el servidor
 app.listen(3002, () => {
